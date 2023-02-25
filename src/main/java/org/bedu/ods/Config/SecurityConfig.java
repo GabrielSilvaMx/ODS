01094eb6ccc3933b22aab2bb497639b97c1bc83f
Gabriel Silva <sv.gabo@gmail.com>
2023-02-25 14:26:59 -0600

    Code smells en SecuritConfig, add TestController

--- src/main/java/org/bedu/ods/Config/SecurityConfig.java
+++ src/main/java/org/bedu/ods/Config/SecurityConfig.java
@@ -23,7 +23,6 @@ import org.bedu.ods.service.JpaUserDetailsService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -33,7 +32,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
--- src/test/java/org/bedu/ods/OdsApplicationTest.java
+++ src/test/java/org/bedu/ods/OdsApplicationTest.java
@@ -1,9 +1,14 @@
 package org.bedu.ods;
 
+import org.bedu.ods.controller.TestController;
 import org.bedu.ods.entity.UsuarioServiceTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.bedu.ods.entity.mapper.UsuariosMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @RunWith(Suite.class)
@@ -17,8 +22,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class OdsApplicationTest {
 
+    @Mock
+    public TestController testController;
+
     @Test
     void contextLoads() {
+        assertThat(testController).isNotNull();
     }
 
 }
